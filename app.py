1a84746838efd4ab10502292143fa2b27485e358
Muhammad Hameed <eng.hameed2943@gmail.com>
2026-09-04 10:07:33 +0500

    fix: force add app.py to resolve Render uvicorn app:app deployment error

---
+++ app.py
@@ -0,0 +1,7 @@
+from src.api import app
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
